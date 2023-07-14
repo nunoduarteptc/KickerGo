60e8b5b0186de482ae444d3163b262e0f19901ef
nunoduarteptc <nuno.duarte@jumia.com>
2023-07-14 14:31:28 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"html/template"
+	"encoding/json"
+	"log"
+	"net/http"
+	"kicker/webSockets"
+)
+
+type Result struct {
+	Id int `json:"id"`
+	Message string `json:"message"`
+}
+
+var messageChannel chan string
+
+func postMessage(w http.ResponseWriter, r *http.Request) {
+	var result Result
+	result.Message = "Recieved message"
+
+	out, err := json.MarshalIndent(result, "", "    ")
+
+	if (err != nil) {
+		log.Println(err)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.Write(out)
+}
+
+
+func home(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("index.html")
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func main() {
+	go webSockets.Init()
+	http.HandleFunc("/home", home)
+	http.HandleFunc("/message", postMessage)
+
+	log.Println("Starting web server on port 8090")
+	http.ListenAndServe(":8090", nil)
+}
+
+
---
+++ webSockets/webSockets.go
@@ -0,0 +1,53 @@
+package webSockets
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket" // Old websocket need to improve
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:1024,
+	WriteBufferSize:1024,
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p ,err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(p))
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool {return true}
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Printf("Client Successfully connected")
+
+	reader(ws)
+}
+
+
+
+func Init() {
+	http.HandleFunc("/ws", wsEndpoint)
+
+	log.Println("Starting web server on port 8091")
+
+	http.ListenAndServe(":8091", nil)
+}
+
